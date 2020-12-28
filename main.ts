5b22c4ab77352fe57212fcfdc40d534a8683bbb1
Barrie <barrie.sharp@hotmail.co.uk>
2020-12-28 14:12:27 +0000

    added stop start

--- main.ts
+++ main.ts
@@ -1,34 +1,49 @@
+input.onButtonPressed(Button.A, function () {
+    Active = !(Active)
+})
+let Active = false
 let FollowDistanceMin = 10
 let FollowDistanceMax = 12
 let TooFar = 25
 let Speed = 30
+Active = false
 basic.forever(function () {
-    if (maqueen.Ultrasonic(PingUnit.Centimeters) < FollowDistanceMin) {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, Speed)
+    if (Active) {
+        if (maqueen.Ultrasonic(PingUnit.Centimeters) < FollowDistanceMin) {
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, Speed)
+            basic.showLeds(`
+                # # # # .
+                # . . . #
+                # # # # #
+                # . # . .
+                # . . # #
+                `)
+        }
+        if (maqueen.Ultrasonic(PingUnit.Centimeters) > FollowDistanceMax && maqueen.Ultrasonic(PingUnit.Centimeters) <= TooFar) {
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, Speed)
+            basic.showLeds(`
+                # # # # #
+                # . . . .
+                # # # . .
+                # . . . .
+                # . . . .
+                `)
+        }
+        if (maqueen.Ultrasonic(PingUnit.Centimeters) >= FollowDistanceMin && maqueen.Ultrasonic(PingUnit.Centimeters) <= FollowDistanceMax) {
+            maqueen.motorStop(maqueen.Motors.All)
+            basic.showIcon(IconNames.Happy)
+        }
+        if (maqueen.Ultrasonic(PingUnit.Centimeters) > TooFar) {
+            maqueen.motorStop(maqueen.Motors.All)
+            basic.showIcon(IconNames.Sad)
+        }
+    } else {
         basic.showLeds(`
-            # # # # .
-            # . . . #
-            # # # # #
-            # . # . .
-            # . . # #
+            . . . # .
+            # . # . #
+            # . # # #
+            # . # . #
+            . . # . #
             `)
-    }
-    if (maqueen.Ultrasonic(PingUnit.Centimeters) > FollowDistanceMax && maqueen.Ultrasonic(PingUnit.Centimeters) <= TooFar) {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, Speed)
-        basic.showLeds(`
-            # # # # #
-            # . . . .
-            # # # . .
-            # . . . .
-            # . . . .
-            `)
-    }
-    if (maqueen.Ultrasonic(PingUnit.Centimeters) >= FollowDistanceMin && maqueen.Ultrasonic(PingUnit.Centimeters) <= FollowDistanceMax) {
-        maqueen.motorStop(maqueen.Motors.All)
-        basic.showIcon(IconNames.Happy)
-    }
-    if (maqueen.Ultrasonic(PingUnit.Centimeters) > TooFar) {
-        maqueen.motorStop(maqueen.Motors.All)
-        basic.showIcon(IconNames.Sad)
     }
 })
